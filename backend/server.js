2a5ddd1de7cb17cc2c8f69507ab4149026038a93
mpvishnu <47482031+mpvishnu@users.noreply.github.com>
2020-03-11 23:23:07 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 let Recipe = require("./models/recipe.js");
 let Review = require("./models/review.js");
 const path = require(“path”);
-app.use(express.static(path.join(__dirname, “client/build”)))
+app.use(express.static(path.join(__dirname, “frontend/build”)))
 
 require("dotenv").config();
 
