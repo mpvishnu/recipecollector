cb2f16e3fb9fa07644ab08841636b9de5ce2ba94
mpvishnu <47482031+mpvishnu@users.noreply.github.com>
2020-03-12 12:05:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const mongoose = require("mongoose");
 let Recipe = require("./models/recipe.js");
 let Review = require("./models/review.js");
 const path = require("path");
-// app.use(express.static(path.join(__dirname, "frontend/build")))
 
 require("dotenv").config();
 
 const app = express();
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -19,17 +17,11 @@ var uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 var connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log("Connected to MongoDB ATLAS Successfully... !");
-// });
 
 connection.on(
   "error",
   console.error.bind(console, "MongoDB connection error:")
 );
-
-// const recipeRouter = require('./routes/getrecipe.js');
-// app.use('/getrecipe', recipeRouter);
 
 app.get("/", (req, res) => {
   res.json({ content: "HELLO FROM SERVER!" });
@@ -44,7 +36,6 @@ app.get("/getrecipes", (req, res) => {
   } else {
     console.log("GET Request Detected...!");
     Recipe.find({}, { _id: 1, Name: 1 })
-      // .then(recipes => console.log(recipes))
       .then(recipes => res.json(recipes))
       .catch(err => res.status(400).json("Error: " + err));
   }
@@ -53,25 +44,12 @@ app.get("/getrecipes", (req, res) => {
 app.post("/savereview", (req, res) => {
   let ipAdd = req.connection.remoteAddress;
   const reviews = req.body.reviews;
-  //   console.log(reviews);
-  //   console.log("IP Address of the user" + ipAdd);
 
   const newReview = new Review({ ipAdd, reviews });
   newReview
     .save()
     .then(() => res.json("Review added!"))
     .catch(err => res.status(400).json("Error: 400"));
-  //res.json("Stored The recipe review")
-});
-
-app.use(express.static("frontend/build"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
 app.listen(port, () => {
